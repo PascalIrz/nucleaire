025f073a52748f80081371d16d3fdb1764146ec4
IRZ Pascal <pascal.irz@afbiodiversite.fr>
2025-03-18 19:32:44 +0100

    ajout tests bivariés booléennes

---
+++ R/mon_chi2.R
@@ -0,0 +1,5 @@
+mon_chi2 <- function(x,y) {
+  tbl <- df %>% select(x,y) %>% table()
+  chisq_pval <- round(chisq.test(tbl)$p.value, 4)
+  cramV <- round(cramersV(tbl), 4) 
+  data.frame(x, y, chisq_pval, cramV) }
