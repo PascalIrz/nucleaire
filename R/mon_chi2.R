bfc5ece7f2ed6eb2637dc6776b916197d3d33839
IRZ Pascal <pascal.irz@afbiodiversite.fr>
2025-03-19 11:13:16 +0100

    Plus de rédaction

--- R/mon_chi2.R
+++ R/mon_chi2.R
@@ -1,5 +1,6 @@
-mon_chi2 <- function(x,y) {
-  tbl <- df %>% select(x,y) %>% table()
+mon_chi2 <- function(x, y) {
+  tbl <- df %>% select(x, y) %>% table()
   chisq_pval <- round(chisq.test(tbl)$p.value, 4)
-  cramV <- round(cramersV(tbl), 4) 
-  data.frame(x, y, chisq_pval, cramV) }
+  cramV <- round(cramersV(tbl), 2)
+  data.frame(x, y, chisq_pval, cramV)
+}
